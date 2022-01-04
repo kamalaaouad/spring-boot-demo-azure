b170b6032478a95607cfe4e1a6b04293310f9bf1
kamalaaouad <kamalaaouad1999@gmail.com>
2022-01-04 22:02:12 +0100

    first commit

---
+++ src/test/java/fr/company/docker/ExampleDockerAwsApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.company.docker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleDockerAwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
